189301617e95200d037c926f150744bf63b7e354
Creotip <creotip@gmail.com>
2021-08-03 11:35:11 +0300

    second

--- tools/plugins/netlify/stop-build/index.js
+++ tools/plugins/netlify/stop-build/index.js
@@ -1,5 +1,11 @@
 module.exports = {
-  onPreBuild: () => {
-    console.log('Hello world from the plugin!')
+  onPreBuild: ({ utils }) => {
+    const currentProject = 'bike-store'
+    const projectHasChanged = false
+    if (!projectHasChanged) {
+      utils.build.cancelBuild(
+        `Build was cancelled because ${currentProject} was not affected by the latest changes`
+      )
+    }
   },
 }
